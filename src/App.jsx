6efbf85d6bd89f8ba2d517fa689a282bc4b467bf
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2026-04-15 20:08:27 +0330

    add sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
+import Testimonial from "./sections/Testimonial";
+
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
