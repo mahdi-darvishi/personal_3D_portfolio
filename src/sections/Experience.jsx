a983c955c6dcf8ba55c9535fba89a641f45fc295
mahdi-darvishi <mehdidarvishi2004@gmail.com>
2026-04-15 20:08:04 +0330

    add exprince

---
+++ src/sections/Experience.jsx
@@ -0,0 +1,11 @@
+import { Timeline } from "../components/Timeline";
+import { experiences } from "../constants";
+const Experience = () => {
+  return (
+    <div className="w-full ">
+      <Timeline data={experiences} />
+    </div>
+  );
+};
+
+export default Experience;
